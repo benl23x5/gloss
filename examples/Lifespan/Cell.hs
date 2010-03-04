0d0758bd0415a4a0ae1ec7bf216f8a034ef6af2f
benl <benl@ouroborus.net>
2010-03-04 15:51:32 +1100

    Add Lifespan example
    Ignore-this: c4d5f6cad04efa505aa9906e440a73ff
    
    darcs-hash:20100304045132-bf82d-86631cd2c113ab12bf0ce59ac2dc097934f52fd0

---
+++ examples/Lifespan/Cell.hs
@@ -0,0 +1,36 @@
+module Cell where
+
+import Graphics.Gloss
+
+data Cell = Cell Point    -- centre
+                 Float    -- radius
+                 Int      -- remaining lifetime
+                 deriving Show
+
+-- Produce a new cell of a certain relative radius at a certain angle.
+-- The factor argument is in the range [0..1] so spawned cells are
+-- smaller than their parent.
+-- The check whether it fits in the community is elsewhere.
+offspring :: Cell -> Float -> Float -> Int -> Cell
+offspring (Cell (x,y) r _) alpha factor lifespan =
+    Cell (x + (childR+r) * cos alpha, y + (childR+r) * sin alpha)
+         childR 
+         lifespan
+    where childR = factor * r
+
+-- Do two cells overlap?         
+-- Used to decide if newly spawned cells can join the community.
+overlap :: Cell -> Cell -> Bool
+overlap (Cell (x1,y1) r1 _) (Cell (x2,y2) r2 _) = centreDist < (r1 + r2) *0.999
+    where centreDist = sqrt(xdiff*xdiff + ydiff*ydiff)
+          xdiff = x1 - x2
+          ydiff = y1 - y2
+
+-- thickness of circle is determined by lifespan
+render :: Cell -> Picture
+render (Cell (x,y) r life) 
+	= Color (makeColor 0.6 z 0.6 1.0)
+	$ Translate x y
+	$ ThickCircle (r - thickness / 2) thickness
+	where	z 		= fromIntegral life * 0.12
+		thickness	= fromIntegral life
---
+++ examples/Lifespan/Community.hs
@@ -0,0 +1,58 @@
+module Community where
+
+import Cell
+import Graphics.Gloss
+
+type Community = [Cell]
+
+-- does a (newly spawned) cell fit in the community?
+-- that is, does it overlap with any others?
+fits :: Cell -> Community -> Bool
+fits cell cells = not $ any (overlap cell) cells
+
+-- For each member of a community, produce one offspring
+-- The lists of Floats are the (random) parameters that determine size
+
+-- and location of each offspring.
+spawn :: Community -> [Float] -> [Float] -> [Int] -> [Cell]
+spawn = zipWith4 offspring
+
+zipWith4 :: (a -> b -> c -> d -> e) -> [a] -> [b] -> [c] -> [d] -> [e]
+zipWith4 f [] _ _ _ = []
+zipWith4 f _ [] _ _ = []
+zipWith4 f _ _ [] _ = []
+zipWith4 f _ _ _ [] = []
+zipWith4 f (b:bs) (c:cs) (d:ds) (e:es) =
+    f b c d e : zipWith4 f bs cs ds es
+
+
+-- Given a collection of cells (one spawned by each member of the
+-- community) check if it fits, and if so add it to the community.
+-- That check must include new cells that have been added to the
+-- community in this process.
+survive :: [Cell] -> Community -> Community
+survive [] comm = comm
+survive (cell:cells) comm
+    | fits cell comm  = survive cells (cell:comm)
+    | otherwise       = survive cells comm
+
+age :: Community -> Community
+age [] = []
+age (Cell c r 0 : cells) = age cells
+age (Cell c r life : cells) = Cell c r (life-1) : age cells
+
+
+-- The next generation of a community
+generation :: Community ->  [Float] -> [Float] -> Community
+generation comm angles scales =
+    survive (spawn comm angles scales (repeat 5)) (age comm)
+
+render :: Community -> Picture
+render comm 
+	= Pictures 
+	$ map Cell.render comm
+
+initial :: Community
+initial = [Cell (0,0) 50 5]
+
+
