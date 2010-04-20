28d20188c92f387687de18623bbcac73d16d099d
benl <benl@ouroborus.net>
2010-04-20 14:22:07 +1000

    Add Machina demo
    Ignore-this: 4e64776a14b7323eee8a368545faabec
    
    darcs-hash:20100420042207-bf82d-a4d0b8f957b0f49c9958a07ec30e037aee6af600

---
+++ examples/Machina/Main.hs
@@ -0,0 +1,29 @@
+
+import Graphics.Gloss
+
+main 	= animateInWindow "deus ex machina" (800, 600) (10, 10) black frame
+
+frame time
+	= Scale 0.8 0.8
+	$ Rotate (time * 30)
+	$ mach time 6
+	
+mach t 0 = leaf
+mach t d
+ = Pictures
+	[ leaf
+	, Translate 0 (-100) 
+		$ Scale 0.8 0.8 
+		$ Rotate (90 + t * 30) 
+		$ mach (t * 1.5) (d - 1)
+
+	, Translate 0   100 
+		$ Scale 0.8 0.8 
+		$ Rotate (90 - t * 30) 
+		$ mach (t * 1.5) (d - 1) ]
+	
+leaf	= Pictures
+		[ Color (makeColor 1.0 1.0 1.0 0.5) $ Polygon loop
+		, Color (makeColor 0.0 0.0 1.0 0.8) $ Line loop ]
+
+loop	= [(-10, -100), (-10, 100), (10, 100), (10, -100), (-10, -100)]
