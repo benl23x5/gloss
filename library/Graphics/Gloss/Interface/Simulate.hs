6b0e198c5563eda6fed3d7b816de39985fd9b6df
Ben Lippmeier <benl@ouroborus.net>
2010-11-23 15:55:08 +1100

    Add missing file
    Ignore-this: aa8f30c286cb53cd1744bcfbc8139efe
    
    darcs-hash:20101123045508-08f32-1ebdeff9ff0257d2876ee40758ed437823927471

---
+++ library/Graphics/Gloss/Interface/Simulate.hs
@@ -0,0 +1,17 @@
+
+-- We export this stuff separately so we don't clutter up the 
+-- API of the Graphics.Gloss module.
+
+-- | Simulate mode is for producing an animation of some model who's picture
+--   changes over finite time steps. The behavior of the model can also depent
+--   on the current `ViewPort`.
+module Graphics.Gloss.Interface.Simulate
+ 	( module Graphics.Gloss.Data.Picture
+	, module Graphics.Gloss.Data.Color
+	, ViewPort(..)
+	, simulateInWindow)
+where
+import Graphics.Gloss.Data.Picture
+import Graphics.Gloss.Data.Color
+import Graphics.Gloss.Internals.Interface.ViewPort
+import Graphics.Gloss.Internals.Interface.Simulate	(simulateInWindow)
