9d4b6954aa46d4fb8e7c1ec1e6889c0f94ea4bb5
benl <benl@ouroborus.net>
2011-10-22 21:19:11 +1100

    Add GameEvent example for testing which events we receive
    Ignore-this: bef6face73e65d3dbb2c53339cb18719
    
    darcs-hash:20111022101911-bf82d-0eae0b4b81d7de7be261c5c0a79160334a4c7a28

---
+++ examples/GameEvent/Main.hs
@@ -0,0 +1,17 @@
+
+import Graphics.Gloss
+
+-- | Display the last event received as text.
+main
+ = gameInWindow
+        "GameEvent"
+        (700, 100)
+        (10, 10)
+        white
+        100
+        ""
+        (\str     -> Translate (-340) 0 $ Scale 0.1 0.1 $ Text str)
+        (\event _ -> show event)
+        (\_ world -> world)
+        
+
