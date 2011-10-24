840c29326335d20f01f8633ee8f4c92ec76c0216
Ben Lippmeier <benl@ouroborus.net>
2011-10-24 13:36:36 +1100

    Add picture drawing example
    Ignore-this: aeb9af22bd198f47a757551576c83c1f
    
    darcs-hash:20111024023636-08f32-3e5b8d2098b23896ed7c550653196de326b2cc63

---
+++ examples/Draw/Main.hs
@@ -0,0 +1,62 @@
+
+-- | Simple picture drawing application. 
+--   Like MSPaint, but you can only draw lines.
+import Graphics.Gloss.Interface.Game
+import Graphics.Gloss
+import Data.Maybe (maybe)
+import Debug.Trace
+
+main 
+ = do   let state = State Nothing []
+        gameInWindow
+                "Draw"
+                (600, 600)
+                (0,0)
+                white
+                100
+                state
+                makePicture
+                handleEvent 
+                stepWorld
+
+-- | The game state.
+data State      
+        = State (Maybe Path)    -- The current line being drawn.
+                [Picture]       -- All the lines drawn previously.
+
+-- | A Line Segment
+type Segment    = ((Float, Float), (Float, Float))
+
+
+-- | Convert our state to a picture.
+makePicture :: State -> Picture
+makePicture (State m xs)
+        = Pictures (maybe xs (\x -> Line x : xs) m)
+
+
+-- | Handle mouse click and motion events.
+handleEvent :: Event -> State -> State
+handleEvent event state
+        -- If the mouse has moved, then extend the current line.
+        | EventMotion (x, y)    <- event
+        , State (Just ps) ss    <- state
+        = State (Just ((x, y):ps)) ss 
+
+        -- Start drawing a new line.
+        | EventKey (MouseButton LeftButton) Down _ pt@(x,y) <- event
+        , State Nothing ss       <- state
+        = State (Just [pt])
+                ((Translate x y $ Scale 0.1 0.1 $ Text "Down") : ss)
+
+        -- Finish drawing a line, and add it to the picture.
+        | EventKey (MouseButton LeftButton) Up _ pt@(x,y)      <- event
+        , State (Just ps) ss    <- state
+        = State Nothing
+                ((Translate x y $ Scale 0.1 0.1 $ Text "up")   : Line (pt:ps) : ss)
+
+        | otherwise
+        = state
+
+
+stepWorld :: Float -> State -> State
+stepWorld _ = id
