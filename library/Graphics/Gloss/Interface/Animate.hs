bec5dd195805ab473ea710b08596f35612100ec2
benl <benl@ouroborus.net>
2011-10-24 11:16:52 +1100

    Add missing interface files
    Ignore-this: 7f955c265aa572dd02de252226d07cd3
    
    darcs-hash:20111024001652-bf82d-bd1fde10446e9b7c9e00b524de4a9f1c150143d4

---
+++ library/Graphics/Gloss/Interface/Animate.hs
@@ -0,0 +1,10 @@
+
+-- | Display mode is for drawing a static picture.
+module Graphics.Gloss.Interface.Animate
+ 	( module Graphics.Gloss.Data.Picture
+	, module Graphics.Gloss.Data.Color
+	, animateInWindow)
+where
+import Graphics.Gloss.Data.Picture
+import Graphics.Gloss.Data.Color
+import Graphics.Gloss.Internals.Interface.Animate
