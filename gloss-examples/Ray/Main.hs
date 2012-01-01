4b93f85cd5ce7a77be9b65df206c6414998edbe6
Ben Lippmeier <benl@ouroborus.net>
2012-01-01 23:15:06 +1100

    ray: tweak starting values for mac air
    Ignore-this: f146bacc60a8c8e1a62e3f935d4a97b4
    
    darcs-hash:20120101121506-08f32-1b6d74b2f77bb96c0cd88bd520a8625973aa3480

--- gloss-examples/Ray/Main.hs
+++ gloss-examples/Ray/Main.hs
@@ -14,15 +14,15 @@ main :: IO ()
 main 
  = do   args    <- getArgs
         case args of
-         []     -> run 720 450 2 400
+         []     -> run 1440 900 5 400
 
          [sizeX, sizeY, zoom, fov]
                 -> run (read sizeX) (read sizeY) (read zoom) (read fov)
 
          _ -> putStr $ unlines
            [ "trace <sizeX::Int> <sizeY::Int> <zoom::Int> (fov::Int)"
-           , "    sizeX, sizeY - visualisation size        (default 720, 450)"
-           , "    zoom         - pixel replication factor  (default 2)"
+           , "    sizeX, sizeY - visualisation size        (default 1440, 900)"
+           , "    zoom         - pixel replication factor  (default 5)"
            , "    fov          - field of view             (default 400)"
            , ""
            , " You'll want to run this with +RTS -N to enable threads" ]
