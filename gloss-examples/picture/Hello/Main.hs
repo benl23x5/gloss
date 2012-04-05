d69f1831a83347514fda39048efeac6dc4f08224
Ben Lippmeier <benl@ouroborus.net>
2012-04-05 19:43:56 +1000

    Really
    Ignore-this: b301648e2f4653f16bcb499fcc781c5d
    
    darcs-hash:20120405094356-08f32-2a3fb7b963977644d827afda9f60e6007b839523

---
+++ gloss-examples/picture/Hello/Main.hs
@@ -0,0 +1,18 @@
+
+-- | Display "Hello World" in a window.
+--
+import Graphics.Gloss
+
+main 	
+ = display 
+        (InWindow
+	       "Hello World" 	 -- window title
+		(400, 150) 	 -- window size
+		(10, 10)) 	 -- window position
+	white			 -- background color
+	picture			 -- picture to display
+
+picture	
+	= Translate (-170) (-20) -- shift the text to the middle of the window
+	$ Scale 0.5 0.5		 -- display it half the original size
+	$ Text "Hello World"	 -- text to display
