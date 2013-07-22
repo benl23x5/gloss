5f484a39972a8c91454fc609ce6b0812197b660c
f <f@mazzo.li>
2013-07-22 23:54:56 +1000

    cosmetics
    Ignore-this: 41b0ee889e1d034057234a1825648d67
    
    darcs-hash:20130722135456-b4eb1-13beae722af003494e13a28557788f92d726bd22

--- gloss/Graphics/Gloss/Internals/Interface/Event.hs
+++ gloss/Graphics/Gloss/Internals/Interface/Event.hs
@@ -23,8 +23,7 @@ keyMouseEvent ::
 	-> (Int, Int)
 	-> IO Event
 keyMouseEvent backendRef key keyState modifiers pos
- = do	pos' <- convertPoint backendRef pos
-	return $ EventKey key keyState modifiers pos'
+	= EventKey key keyState modifiers <$> convertPoint backendRef pos
 
 motionEvent ::
 	forall a . Backend a
@@ -40,14 +39,14 @@ convertPoint ::
 	-> (Int, Int)
 	-> IO (Float,Float)
 convertPoint backendRef pos
- = do	(sizeX_, sizeY_) 		<- getWindowDimensions backendRef
-	let (sizeX, sizeY)		= (fromIntegral sizeX_, fromIntegral sizeY_)
+ = do	(sizeX_, sizeY_) 	<- getWindowDimensions backendRef
+	let (sizeX, sizeY)	= (fromIntegral sizeX_, fromIntegral sizeY_)
 
-	let (px_, py_)	= pos
-	let px		= fromIntegral px_
-	let py		= sizeY - fromIntegral py_
+	let (px_, py_)		= pos
+	let px			= fromIntegral px_
+	let py			= sizeY - fromIntegral py_
 	
-	let px'		= px - sizeX / 2
-	let py' 	= py - sizeY / 2
-	let pos'	= (px', py')
+	let px'			= px - sizeX / 2
+	let py' 		= py - sizeY / 2
+	let pos'		= (px', py')
 	return pos'
