298ec09ebef89fa7d98c5a7c8bbfc337cf622444
Nenad <ahhhhmed@gmail.com>
2016-12-12 14:22:21 +0100

    Made a triangulation function (that does nothing for now).

--- gloss/Graphics/Gloss/Geometry.hs
+++ gloss/Graphics/Gloss/Geometry.hs
@@ -1,8 +1,9 @@
 
 module Graphics.Gloss.Geometry
 	( module Graphics.Gloss.Geometry.Angle
-	, module Graphics.Gloss.Geometry.Line )
+	, module Graphics.Gloss.Geometry.Line
+	, module Graphics.Gloss.Geometry.Triangulation )
 where
 import Graphics.Gloss.Geometry.Angle
 import Graphics.Gloss.Geometry.Line
-	
+import Graphics.Gloss.Geometry.Triangulation
