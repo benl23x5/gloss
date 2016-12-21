b33733b51e0cb113737e2fd63f6ac042a9fc2b98
Nenad <ahhhhmed@gmail.com>
2016-12-21 21:25:59 +0100

    implemented accumulatorFunction

--- gloss/Graphics/Gloss/Geometry/Triangulation.hs
+++ gloss/Graphics/Gloss/Geometry/Triangulation.hs
@@ -15,25 +15,26 @@ data Edge
         { start :: Point
         , end :: Point }
         deriving (Show)
-data Event
-        = Event
-        { eventCoordinates :: Point
-        , startOf :: Set.Set Edge
-        , intersectionOf :: Set.Set Edge
-        , endOf :: Set.Set Edge }
-        deriving (Show)
+
+type Events = Map.Map Point (Set.Set Edge, Set.Set Edge)
+-- data Event
+--         = Event
+--         { eventCoordinates :: Point
+--         , startOf :: Set.Set Edge
+--         , endOf :: Set.Set Edge }
+--         deriving (Show)
 
 
 
-instance Eq Event where
-        Event {eventCoordinates= p0} == Event {eventCoordinates= p1} = p0 == p1
+-- instance Eq Event where
+--         Event {eventCoordinates= p0} == Event {eventCoordinates= p1} = p0 == p1
 
-instance Ord Event where
-        Event {eventCoordinates=(ax,ay)} < Event {eventCoordinates=(bx,by)} = if by == ay then ax < bx else ay < by
-        compare x y
-                | x == y = EQ
-                | x < y = LT
-                | otherwise = GT
+-- instance Ord Event where
+--         Event {eventCoordinates=(ax,ay)} < Event {eventCoordinates=(bx,by)} = if by == ay then ax < bx else ay < by
+--         compare x y
+--                 | x == y = EQ
+--                 | x < y = LT
+--                 | otherwise = GT
 
 instance Eq Edge where
         Edge{start = p0, end = p1} == Edge{start = p2, end = p3}
@@ -53,9 +54,13 @@ instance Ord Edge where
 
 
 -- Finds intersections of two Edges.
-intersection :: Edge -> Edge -> Maybe Point
-intersection Edge{start = p0, end = p1}  Edge{start = p2, end = p3}
-        = intersectSegSeg p0 p1 p2 p3
+intersection :: Maybe Edge -> Maybe Edge -> Maybe Point
+intersection (Just Edge{start = p0, end = p1}) (Just Edge{start = p2, end = p3})
+                | Just x <- intersectSegSeg p0 p1 p2 p3
+                , (x /= p0) && (x /= p1) && (x /= p2) && ( x /= p3)
+                = Just x
+                | otherwise = Nothing
+intersection _ _ = Nothing
 
 makeInitialVertexSet :: Path -> Vertices
 makeInitialVertexSet [] = undefined
@@ -66,31 +71,113 @@ makeInitialVertexSet list@(_:xs) = foldr accumulator Map.empty ( zip list (xs ++
 
 
 
-initialEventSet :: Vertices -> Set.Set Event
-initialEventSet = Map.foldrWithKey event Set.empty
+initialEvents :: Vertices -> Events
+initialEvents = Map.foldrWithKey event Map.empty
 
-event :: Point -> Set.Set Point -> Set.Set Event -> Set.Set Event
+event :: Point -> Set.Set Point -> Events -> Events
 
 event p@(px,py) neighbours events  = let outCompare (x,y) = if px == x then py > y else px < x
                                          edgeStart point = Edge {start = p, end = point}
                                          edgeEnd point = Edge {end = p, start = point}
                                          startSet = Set.map edgeStart (Set.filter outCompare neighbours)
                                          endSet = Set.map edgeEnd (Set.filter (not.outCompare) neighbours)
-                                    in Set.insert Event{eventCoordinates = p, startOf = startSet,
-                                                            intersectionOf = Set.empty, endOf = endSet} events
+                                    in Map.insert p  (startSet, endSet) events
 
-traverseVertices :: ((a, Set.Set b) -> b -> (a, Set.Set b)) -> a -> Set.Set b -> a
-traverseVertices _ acc set | Set.null set = acc
-traverseVertices fn acc set = let ( minElement, withoutMin) = Set.deleteFindMin set
-                                  ( newAcc, newSet) = fn  (acc, withoutMin) minElement
+traverseVertices :: ((a, Map.Map b c) -> (b, c) -> (a, Map.Map b c)) -> a -> Map.Map b c -> a
+traverseVertices _ acc dictionary | Map.null dictionary = acc
+traverseVertices fn acc dictionary = let ( minElement, withoutMin) = Map.deleteFindMin dictionary
+                                         ( newAcc, newSet) = fn  (acc, withoutMin) minElement
                                 in traverseVertices fn  newAcc newSet
--- TODO stopped here :D
--- type accumulatorType = (Set.Set Edge, Set.Set Vertex)
---
--- accumulatorFunction :: (accumulatorType , Set.Set Event) -> Event -> (accumulatorType, Set.Set Event)
--- accumulatorFunction ((edges, vertices), events) event = let deletedEdges = edges `difference` (startOf event `union` endOf event)
---
 
+
+
+type AccumulatorType = (Set.Set Edge, Vertices)
+
+accumulatorFunction :: (AccumulatorType , Events) -> (Point, (Set.Set Edge, Set.Set Edge)) -> (AccumulatorType, Events)
+
+accumulatorFunction ((edges, vertices), events) (_, (startOf, endOf)) = let deletedEdges = edges `Set.difference` (startOf  `Set.union` endOf)
+                                                                            minElem = lookupMin startOf
+                                                                            maxElem = lookupMax startOf
+                                                                            minElemLT 
+                                                                                | Just element <- minElem = Set.lookupLT element deletedEdges
+                                                                                | otherwise = Nothing
+                                                                            maxElemGT 
+                                                                                | Just element <- maxElem = Set.lookupGT element deletedEdges
+                                                                                | otherwise = Nothing
+                                                                            dIntersect = intersection minElem minElemLT
+                                                                            uIntersect = intersection maxElem maxElemGT
+                                                                            event1 = replaceEdge minElem dIntersect events
+                                                                            event2 = replaceEdge maxElem uIntersect event1
+                                                                            event3 = insertEvent dIntersect minElem minElemLT event2
+                                                                            event4 = insertEvent uIntersect maxElem maxElemGT event3
+                                                                            edges1 = deletedEdges `Set.union` difference startOf minElem maxElem
+                                                                            edges2 = addMaybeEdge minElem dIntersect $ addMaybeEdge maxElem uIntersect edges1
+                                                                        in ((edges2, addVertex endOf vertices), event4)
+
+addVertex :: Set.Set Edge -> Vertices -> Vertices
+addVertex edges vertices  
+                    | null edges = vertices
+                    | otherwise = Set.foldr accumulator vertices edges
+                                    where accumulator Edge{start = p, end = q} acc =
+                                                let insert p1 q1 = Map.insertWith Set.union p1 (Set.singleton q1) 
+                                                in insert p q (insert q p acc)
+
+
+difference :: Set.Set Edge -> Maybe Edge -> Maybe Edge -> Set.Set Edge
+
+difference edges edge1 edge2 = let delete e s 
+                                    |Just x <- e = Set.delete x s
+                                    |otherwise = s 
+                               in delete edge1 $ delete edge2 edges 
+
+
+addMaybeEdge :: Maybe Edge -> Maybe Point -> Set.Set Edge -> Set.Set Edge
+
+addMaybeEdge (Just Edge{end = p1}) (Just p) edges = Set.insert Edge{start=p, end = p1} edges
+addMaybeEdge (Just x) Nothing edges = Set.insert x edges
+addMaybeEdge _ _ edges = edges
+
+insertEvent :: Maybe Point -> Maybe Edge -> Maybe Edge -> Events -> Events
+insertEvent (Just p) (Just Edge{start = p0, end = p1}) (Just Edge{start = p2, end = p3}) events 
+            = let startOf = Set.insert Edge{start = p, end = p3} $ Set.insert Edge{start = p, end = p1} Set.empty
+                  endOf = Set.insert Edge{start = p0, end = p} $ Set.insert Edge{start = p2, end = p} Set.empty
+              in Map.insertWith (\(x,y) (a, b) -> (x `Set.union` a, y `Set.union` b)) p (startOf, endOf) events
+insertEvent _ _ _ events = events           
+
+replaceEdge :: Maybe Edge -> Maybe Point -> Events -> Events
+
+replaceEdge (Just edge@Edge{start = p0, end = p1}) (Just p) events = let e = replaceEndOf p0 edge Edge{start = p0, end = p} events
+                                                                     in replaceStartOf p1 edge Edge{start = p, end = p1} e
+replaceEdge _ _ events = events
+
+replaceStartOf :: Point -> Edge -> Edge -> Events -> Events
+-- e1 brisem e2 dodajem
+replaceStartOf p e1 e2 events = let (startOf, endOf) = events Map.! p
+                                    newStartOf = Set.insert e2 $ Set.delete e1 startOf
+                                in Map.insert p  (newStartOf, endOf) events
+
+replaceEndOf :: Point -> Edge -> Edge -> Events -> Events
+-- e1 brisem e2 dodajem
+replaceEndOf p e1 e2 events = let (startOf, endOf) = events Map.! p
+                                  newEndOf = Set.insert e2 $ Set.delete e1 endOf
+                              in Map.insert p  (startOf, newEndOf) events
+
+
+lookupMin :: Set.Set a -> Maybe a 
+lookupMin set
+        | Set.null set = Nothing
+        | otherwise = Just $ Set.findMin set
+
+
+lookupMax :: Set.Set a -> Maybe a 
+lookupMax set
+        | Set.null set = Nothing
+        | otherwise = Just $ Set.findMax set
+
+
+polygonGraph :: Path -> Vertices
+
+polygonGraph l = snd $ traverseVertices accumulatorFunction ( Set.empty, Map.empty) $ initialEvents (makeInitialVertexSet l)
 
 
 triangulate :: Path -> [Picture]
