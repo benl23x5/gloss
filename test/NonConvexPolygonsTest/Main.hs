7eece1a5080de0d1e6b90707a8c7f83d31026c8a
Nenad <ahhhhmed@gmail.com>
2017-01-09 23:31:32 +0100

    Updated test example.

--- test/NonConvexPolygonsTest/Main.hs
+++ test/NonConvexPolygonsTest/Main.hs
@@ -15,18 +15,32 @@ prismSide = 1.0
 prismHeight :: Float
 prismHeight = prismSide *  sqrt 3.0 / 2.0
 
+-- prismPath :: [(Float, Float)]
+-- prismPath = [ (0.0, prismHeight)
+--             , (prismSide * 0.3, 0)
+--             , (prismSide, 0)
+--             , (prismSide * 0.4, - prismHeight * 0.4)
+--             , (prismSide * 0.6, - prismHeight)
+--             , (0 , - prismHeight * 0.5)
+--             , (-prismSide * 0.6, - prismHeight)
+--             , (-prismSide * 0.4, - prismHeight * 0.4)
+--             , (-prismSide, 0)
+--             , (-prismSide * 0.3, 0)
+--             ]
+
 prismPath :: [(Float, Float)]
-prismPath = [ (0.0, prismHeight)
-            , (prismSide * 0.3, 0)
-            , (prismSide, 0)
-            , (prismSide * 0.4, - prismHeight * 0.4)
-            , (prismSide * 0.6, - prismHeight)
-            , (0 , - prismHeight * 0.5)
-            , (-prismSide * 0.6, - prismHeight)
-            , (-prismSide * 0.4, - prismHeight * 0.4)
-            , (-prismSide, 0)
-            , (-prismSide * 0.3, 0)
+prismPath = [ (prismSide * (-0.6), prismHeight * 0.7) -- 1
+            , (prismSide * 0.7, prismHeight * 0.7) -- 2
+            , (prismSide * 0.7, prismHeight * (-0.7)) -- 3
+            , (prismSide * (-0.7), prismHeight * (-0.7)) -- 4
+            , (prismSide * (-0.7), prismHeight * 0.6) -- 5
+            , (prismSide * (-0.5), prismHeight * 0.4) -- 6
+            , (prismSide * (-0.5), prismHeight * (-0.5)) -- 7
+            , (prismSide * 0.5, prismHeight * (-0.5)) -- 8
+            , (prismSide * 0.5, prismHeight * 0.5) -- 9
+            , (prismSide * (-0.4), prismHeight * 0.5) -- 10
             ]
+
 
 drawing :: Picture
 drawing = scale 200 200 $
