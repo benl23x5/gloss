be029f0ac777a4620d59348549d11b9f665879ff
Ben Lippmeier <benl@ouroborus.net>
2019-12-28 22:06:58 +1100

    sdl: get high dpi mode to work

--- gloss-sdl/README.md
+++ gloss-sdl/README.md
@@ -10,16 +10,20 @@ The current Gloss library suffers from the following problems, which we try to a
 
 2. Gloss can also use GLFW for window management, but still relies on GLUT for drawing vector text. SDL_ttf provides its own text support.
 
-3. SDL_ttf can draw true type fonts, rather than just the ugly vector fonts of GLUT.
+3. GLUT supports only a single vector font, while SDL_ttf can draw text using standard true type font files.
 
 4. Once control enters the GLUT main loop it does not escape. GLUT redraws can happen on window events or at a fixed timer expiration. Redraws cannot be triggered by external events such as file or network activity. The current Gloss displayIO interface tries to hack around this limitation, but the interface still only updates static pictures once per second. Using the SDL API one can define their own main loop and trigger redraws based on arbitrary events.
 
 5. GLUT does not support high dpi displays (like Retina displays on Mac). GLFW seems to have some support, but the Gloss authors haven't tested it yet. SDL has working and maintained high DPI display support.
 
-6. The Gloss picture type includes a polygon constructor, which allows polygons to be defined by a path, but there is an unchecked precondition that the polygon must be convex. Trying to defined non-convex paths results in undefined behaviour. There was a project to add triangulation support to gloss but the resulting code was deemed too complex to be merged. In the move to CSG we can present primitive triangles and rectangles as polygons and avoid general polygons.
+6. Gloss has a variety of problems when launched from the GHCi console, ranging from crashes, low framerates, or force quit of GHCi when the window is closed. The behaviour is variable depending on the platform. The SDL event framework allows the window to be closed without quiting the host executable.
 
-7. The Gloss picture type includes circle and arc constructors. The rasterization code for circles and arcs has been a bug farm over the years, and still has known problems. Moving to CSG/ray tracing style rendering will allow us to draw circles and arcs at full resolution in a way that hopefully avoids rasterization bugs.
+7. The Gloss picture type includes a polygon constructor, which allows polygons to be defined by a path, but there is an unchecked precondition that the polygon must be convex. Trying to defined non-convex paths results in undefined behaviour. There was a project to add triangulation support to gloss but the resulting code was deemed too complex to be merged. In the move to CSG we can present primitive triangles and rectangles as polygons and avoid general polygons.
 
-8. The Gloss interface API provides both "pure" and "IO" versions of top-level functions like "display" and "simulate". If we can separate the picture and rendering API cleanly from the window management functionality then we could provide just the "pure" interfaces for simple use-cases. We would require users to use a lower level IO based API for IO driven applications.
+8. The Gloss picture type includes circle and arc constructors. The rasterization code for circles and arcs has been a bug farm over the years, and still has known problems. Moving to CSG/ray tracing style rendering will allow us to draw circles and arcs at full resolution in a way that hopefully avoids rasterization bugs.
 
-9. Gloss does not have sound support, and there appears to be no simple graphics/sound interface in the Haskell ecosystem. Attempts to combine separate audio libraries with gloss usually end up with latency problems, resulting in auto clicking.
+9. The Gloss interface API provides both "pure" and "IO" versions of top-level functions like "display" and "simulate". If we can separate the picture and rendering API cleanly from the window management functionality then we could provide just the "pure" interfaces for simple use-cases. We would require users to use a lower level IO based API for IO driven applications.
+
+10. Gloss does not have sound support, and there appears to be no simple graphics/sound interface in the Haskell ecosystem. Attempts to combine separate audio libraries with gloss usually end up with latency problems, resulting in auto clicking.
+
+
--- gloss-sdl/src/main.c
+++ gloss-sdl/src/main.c
@@ -5,58 +5,160 @@
 
 int main(int argc, char** argv)
 {
-        SDL_Window      *window   = 0;
-        SDL_Renderer    *renderer = 0;
-        SDL_Texture     *texture  = 0;
-        SDL_Surface     *surface  = 0;
+        SDL_version     version;
+        SDL_GetVersion  (&version);
+        printf  ( "SDL version = %d.%d.%d\n"
+                , version.major, version.minor, version.patch);
 
         size_t  winSizeX        = 600;
         size_t  winSizeY        = 600;
 
-        // Init video subsystem.
+        // ------------------------------------------------
+        // Initialize the video subsystem.
         assert (SDL_Init (SDL_INIT_VIDEO) == 0);
         assert (SDL_InitSubSystem (SDL_INIT_VIDEO) == 0);
 
-        // Open the main window.
-        window  = SDL_CreateWindow
+        int numDisplays = SDL_GetNumVideoDisplays();
+        for (int iDisplay = 0; iDisplay < numDisplays; iDisplay++)
+        {       float ddpi, hdpi, vdpi;
+                SDL_Rect bounds;
+
+                assert  (SDL_GetDisplayDPI    (iDisplay, &ddpi, &hdpi, &vdpi) == 0);
+                assert  (SDL_GetDisplayBounds (iDisplay, &bounds) == 0);
+
+                printf  ("display %d\n", iDisplay);
+
+                printf  ("  dpi      = { .ddpi = %3.2f, .hdpi = %3.2f, .vdpi = %3.2f }\n"
+                        , ddpi, hdpi, vdpi);
+
+                printf  ("  bounds   = { .x = %d, .y = %d, .w = %d, .h = %d }\n"
+                        , bounds.x, bounds.y, bounds.w, bounds.h);
+
+                int numDisplayModes = SDL_GetNumDisplayModes (iDisplay);
+                for (int iMode = 0; iMode < numDisplayModes; iMode++)
+                {       SDL_DisplayMode mode;
+                        assert  (SDL_GetDisplayMode(iDisplay, iMode, &mode) == 0);
+                        printf  ("  mode %2d  = { .w = %4d, .h = %4d, .format = %s }\n"
+                                , iMode
+                                , mode.w, mode.h
+                                , SDL_GetPixelFormatName(mode.format));
+                }
+        }
+
+
+        // ------------------------------------------------
+        // Create the main window.
+        //   Allowing high DPI windows will cause windows opened on a
+        //   high DPI display to have a larger drawable size than the
+        //   abstract "window size" that we have specified in screen
+        //   coordinates.
+        SDL_Window* window
+         = SDL_CreateWindow
                 ( "Test"                        // window title.
                 , SDL_WINDOWPOS_UNDEFINED       // x position of window.
                 , SDL_WINDOWPOS_UNDEFINED       // y position of window.
                 , winSizeX, winSizeY            // window size.
-                , SDL_WINDOW_RESIZABLE);
+                , SDL_WINDOW_ALLOW_HIGHDPI);
         assert (window != 0);
 
+        // Check reported size of window.
+        //   This should match winSizeX/Y, even for high DPI windows.
+        int     winSizeReportedX = 0;
+        int     winSizeReportedY = 0;
+        SDL_GetWindowSize (window, &winSizeReportedX, &winSizeReportedY);
+        printf ("window size\n");
+        printf ("  reported = { .w = %d, .h = %d }\n", winSizeReportedX, winSizeReportedY);
+        assert (  winSizeX == winSizeReportedX
+               && winSizeY == winSizeReportedY);
+
+        // Check drawable size of window.
+        //   If we have a high DPI window then this will be larger than the
+        //   winSizeX/Y that we asked for when we created it.
+        int     winSizeDrawableX = 0;
+        int     winSizeDrawableY = 0;
+        SDL_GL_GetDrawableSize(window, &winSizeDrawableX, &winSizeDrawableY);
+        printf  ( "  drawable = { .w = %d, .h = %d }\n"
+                , winSizeDrawableX, winSizeDrawableY);
+
+
+        // ------------------------------------------------
+        // List the available rendererers.
+        int     numRenderDrivers = SDL_GetNumRenderDrivers();
+        for (int i = 0; i < numRenderDrivers; i++)
+        {       SDL_RendererInfo info;
+                assert  (SDL_GetRenderDriverInfo(i, &info) == 0);
+                printf  ( "renderer %d = \"%s\"\n"
+                        , i, info.name);
+        }
+
         // Create a new renderer context for the window.
-        renderer = SDL_CreateRenderer
+        SDL_Renderer* renderer
+         = SDL_CreateRenderer
                 ( window                        // attach to this window.
                 , -1                            // use first available driver.
-                , SDL_RENDERER_SOFTWARE);
+                , SDL_RENDERER_ACCELERATED);
         assert (renderer != 0);
 
-        // Make a new texture buffer for the window.
-        texture = SDL_CreateTexture
+        // Check output size of the renderer.
+        //   For high DPI windows this will be either the original winSizeX/Y
+        //   or the larger winSizeDrawableX/Y depending on the renderer.
+        //   On OSX only the accelerated renderers produce High DPI output.
+        int     outSizeReportedX = 0;
+        int     outSizeReportedY = 0;
+        SDL_GetRendererOutputSize
+                (renderer, &outSizeReportedX, &outSizeReportedY);
+        printf  ("output size\n");
+        printf  ("  reported = %d, %d\n"
+                , outSizeReportedX, outSizeReportedY);
+
+
+        // ------------------------------------------------
+        // Make a new texture buffer to back the window.
+        //
+        //   The texture buffer should have the same size as the renderer
+        //   output size. In the case where we got a high DPI window,
+        //   but a software renderer with a lower output size, we still
+        //   want use the lower ouptut size for the texture, otherwise we
+        //   will waste time creating pixels that will just be discarded
+        //   when the texture is copied to the screen.
+        //
+        //   The pixel format is set to ARGB8888 as in the current version
+        //   of SDL that seems to be the native format for every display mode
+        //   (obtained with SDL_GetDisplayMode above).
+        SDL_Texture *texture
+         = SDL_CreateTexture
                 ( renderer
                 , SDL_PIXELFORMAT_ARGB8888
                 , SDL_TEXTUREACCESS_STREAMING
-                , winSizeX, winSizeY);
+                , outSizeReportedX, outSizeReportedY);
         assert (texture != 0);
 
         // Make a RGB surface that we can write into.
-        surface = SDL_CreateRGBSurface
-                ( 0
-                , winSizeX, winSizeY
-                , 32
-                , 0x00ff0000
-                , 0x0000ff00
-                , 0x000000ff
-                , 0xff000000);
+        SDL_Surface *surface
+         = SDL_CreateRGBSurface
+                ( 0             // flags are unused and set to 0.
+                , outSizeReportedX, outSizeReportedY
+                , 32            // bit depth
+                , 0x00ff0000    // red mask
+                , 0x0000ff00    // blue mask
+                , 0x000000ff    // green mask
+                , 0xff000000);  // alpha mask
         assert (surface != 0);
 
-        // Spin in a loop drawing random rectangles.
+
+        // ------------------------------------------------
+        // The main drawing loop.
+
+        // Event counter.
+        int iEvent = 0;
+
         while (1) {
                 SDL_Event event;
 
-                SDL_PollEvent(&event);
+                SDL_WaitEvent(&event);
+                iEvent++;
+                printf ("event %d\n", iEvent);
+
                 if (event.type == SDL_QUIT)
                         break;
 
@@ -66,11 +168,11 @@ int main(int argc, char** argv)
                         , &surface->pixels
                         , &surface->pitch);
 
-                double posX   = 300;
-                double posY   = 300;
+                double posX   = outSizeReportedX / 2;
+                double posY   = outSizeReportedY / 2;
 
-                for (size_t y = 0; y < winSizeY; y++)
-                for (size_t x = 0; x < winSizeX; x++)
+                for (size_t y = 0; y < outSizeReportedX; y++)
+                for (size_t x = 0; x < outSizeReportedY; x++)
                 {
                         double xd = posX - (double)x;
                         double yd = posY - (double)y;
@@ -80,7 +182,7 @@ int main(int argc, char** argv)
                         uint32_t* buf
                          = (uint32_t*) ((uint8_t*)surface->pixels + y * surface->pitch + x * 4);
 
-                        if (d < 100)
+                        if (d < outSizeReportedX / 4)
                                 *buf = 0xff8080ff;
                         else    *buf = 0x00000000;
                 }
