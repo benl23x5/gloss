6677fef9210791250cf72b094f92acb34ff88600
Eivind Jahren <eivind.jahren@gmail.com>
2022-10-02 14:43:41 +0200

    Simplify Lifespan example

--- gloss-examples/picture/Lifespan/Main.hs
+++ gloss-examples/picture/Lifespan/Main.hs
@@ -1,4 +1,3 @@
-
 -- Adapted from ANUPlot version by Clem Baker-Finch
 module Main where
 import qualified World as W
@@ -7,12 +6,11 @@ import System.Random
 
 -- varying prng sequence
 main :: IO ()
-main
- = do   gen <- getStdGen
-        simulate (InWindow "Lifespan" (800, 600) (10, 10))
-                 (greyN 0.1)        -- background color
-                 2                  -- number of steps per second
-                 (W.genesis' gen)   -- initial world
-                 W.render           -- function to convert world to a Picture
-                 W.evolve           -- function to step the world one iteration
-
+main = do
+  gen <- getStdGen
+  simulate (InWindow "Lifespan" (800, 600) (10, 10))
+    (greyN 0.1)     -- background color
+    2               -- number of steps per second
+    (W.genesis gen) -- initial world
+    W.render        -- function to convert world to a Picture
+    W.evolve        -- function to step the world one iteration
